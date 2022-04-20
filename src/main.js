a7e41ffd5876217929bd1626d0028e778cfbb3d4
Xia Wang <18770913380@163.com>
2022-04-20 13:39:56 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 引入全局样式
 import '@/styles/index.less'
+
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
+// 注册使用vant组件库
+Vue.use(Vant);
 
 Vue.config.productionTip = false
 
